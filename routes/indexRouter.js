224ce36708076410b4152979f3d2e311fbb5be6f
Esther Ndosi <estherndosi24@gmail.com>
2026-06-26 12:28:04 +0300

    Added message details feature and addding new message with id

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 
 const messages = [
   {
+    id: 1,
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
   },
   {
+    id: 2,
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
@@ -24,12 +26,20 @@ indexRouter.get("/new", (req, res) => {
 indexRouter.post("/new", (req, res) => {
   const messageUser = req.body.name;
   const messageText = req.body.message;
+  const id = messages.length + 1;
   messages.push({
+    id: id,
     text: messageText,
     user: messageUser,
     added: new Date(),
   });
   res.redirect("/");
+});
+
+indexRouter.get("/messages/:id", (req, res) => {
+  const { id } = req.params;
+  const message = messages.find((message) => message.id === Number(id));
+  res.render("messageDetails", { message: message });
 });
 
 module.exports = indexRouter;
